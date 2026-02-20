12c70e8556740950456c636b3e6159b687ea99d3
Piotr Chmielowiec <chpiotr06@gmail.com>
2026-02-20 16:55:33 +0100

    fix: adjusted behaviour of log in and open app buttons

--- src/guards/is-authenticated.ts
+++ src/guards/is-authenticated.ts
@@ -4,7 +4,7 @@ import { redirect } from "@tanstack/react-router";
 export const isAuthenticatedGuard = async ({
 	context,
 }: { context: RouterContext }) => {
-	const isAuthenticated = await context.auth.isAuthenticated();
+	const isAuthenticated = await context.auth.isAuthenticatedWithReauth();
 	if (!isAuthenticated) {
 		throw redirect({
 			to: "/login",
